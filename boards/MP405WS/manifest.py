0caf8b54e43dab225e45438c7e90dee3426ba307
呆瓜云 <meekdai@163.com>
2021-12-25 14:45:37 +0800

    Update manifest.py

--- boards/MP405WS/manifest.py
+++ boards/MP405WS/manifest.py
@@ -1,2 +1,2 @@
 include("$(MPY_DIR)/extmod/uasyncio/manifest.py")
-freeze("$(BOARD_DIR)/modules", ("htmlserver.py", "logging.py", "tmc5130.py", "ws.py"))
+freeze("$(BOARD_DIR)/modules", ("logging.py"))
