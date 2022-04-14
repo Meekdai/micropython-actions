6ae9b2c312ad55c6fdc288a0279ba1222063e746
呆瓜云 <meekdai@163.com>
2022-04-14 09:53:19 +0800

    add mypico.h

--- boards/MYPICO/mpconfigboard.cmake
+++ boards/MYPICO/mpconfigboard.cmake
@@ -1,1 +1,6 @@
 # cmake file for Raspberry Pi Pico
+# See also: https://github.com/raspberrypi/pico-sdk/tree/master/src/boards/include/boards
+set(PICO_BOARD_HEADER_DIRS ${MICROPY_PORT_DIR}/boards/${MICROPY_BOARD})
+
+# Freeze board.py
+# set(MICROPY_FROZEN_MANIFEST ${MICROPY_BOARD_DIR}/manifest.py)
